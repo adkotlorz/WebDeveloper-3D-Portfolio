917e863fbed845290ef1e5d395fcb04d26a609de
adkotlorz <ad.kotlorz@gmail.com>
2025-06-19 06:05:43 +0200

    showcase bental grid with gsap animations done

--- src/sections/Showcase.jsx
+++ src/sections/Showcase.jsx
@@ -1,10 +1,49 @@
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger)
+
 const Showcase = () => {
+	const sectionRef = useRef(null);
+
+	const projectOneRef = useRef(null);
+	const projectTwoRef = useRef(null);
+	const projectThreeRef = useRef(null);
+
+	useGSAP(() => {
+		const projects = [projectOneRef.current, projectTwoRef.current, projectThreeRef.current];
+
+		gsap.fromTo(sectionRef.current, {
+			opacity: 0
+		}, {
+			opacity: 1,
+			duration: 1.5
+		})
+
+		projects.forEach((card, index) => {
+			gsap.fromTo(card, {
+				y: 50,
+				opacity: 0,
+			}, {
+				y: 0,
+				opacity: 1,
+				duration: 1,
+				delay: 0.3 * (index + 1),
+				scrollTrigger: {
+					trigger: card,
+					start: "top bottom -= 100"
+				}
+			});
+		});
+	}, []);
+
 	return (
-		<div id="work" className="app-showcase">
+		<section ref={sectionRef} id="work" className="app-showcase">
 			<div className="w-full">
 				<div className="showcaselayout">
-				{/*	Left side */}
-					<div className="first-project-wrapper">
+					<div ref={projectOneRef} className="first-project-wrapper">
 						<div className="image-wrapper">
 							<img src="/images/project1.png" alt="ryde"/>
 						</div>
@@ -17,15 +56,14 @@ const Showcase = () => {
 							</p>
 						</div>
 					</div>
-				{/*	Right side*/}
 					<div className="project-list-wrapper overflow-hidden">
-						<div className="project">
+						<div ref={projectTwoRef} className="project">
 							<div className="image-wrapper bg-[#FFEFEB]">
 								<img src="/images/project2.png" alt="Library Management" />
 							</div>
 							<h2>Library Management Platform</h2>
 						</div>
-						<div className="project">
+						<div ref={projectThreeRef} className="project">
 							<div className="image-wrapper bg-[#FFE7EB]">
 								<img src="/images/project3.png" alt="YC Directory" />
 							</div>
@@ -34,7 +72,7 @@ const Showcase = () => {
 					</div>
 				</div>
 			</div>
-		</div>
+		</section>
 	);
 };
 
