72789ef938091eb380e01863fc78687658123965
adkotlorz <ad.kotlorz@gmail.com>
2025-06-19 06:23:38 +0200

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Hero, Showcase } from "./sections/index.js";
+import { Navbar } from "./components/index.js";
 
 const App = () => {
 	return (
 		<>
+			<Navbar />
 			<Hero />
 			<Showcase />
 		</>
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import HeroExperience from './hero_models/HeroExperience';
 import HeroLights from './hero_models/HeroLights';
 import Particles from './hero_models/Particles';
 import AnimatedCounter from './AnimatedCounter';
+import Navbar from './Navbar';
 
-export { Button, HeroExperience, HeroLights, Particles, AnimatedCounter };
+export { Button, HeroExperience, HeroLights, Particles, AnimatedCounter, Navbar };
